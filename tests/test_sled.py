41f159445cb041a8ad265ad1f043c51b96d5f3b6
Tpt <thomaspt@hotmail.fr>
2020-08-09 00:04:27 +0200

    Improves Sled tests

--- tests/test_sled.py
+++ tests/test_sled.py
@@ -1,25 +1,25 @@
 import shutil
 import unittest
 
-from rdflib import RDF, Namespace, Literal, XSD, Graph
+from rdflib import RDF, Namespace, Literal, XSD, Graph, ConjunctiveGraph
 
 EX = Namespace("http://example.com/")
 
 
 class StoreTestCase(unittest.TestCase):
     def test_sled_store_default(self):
-        g = Graph("OxSled")
+        g = ConjunctiveGraph("OxSled")
         self._fill_graph(g)
         self._test_graph(g)
 
     def test_sled_store_open(self):
-        g = Graph("OxSled")
+        g = ConjunctiveGraph("OxSled")
         g.open("test_sled")
         self._fill_graph(g)
         g.close()
         del g
 
-        g = Graph("OxSled")
+        g = ConjunctiveGraph("OxSled")
         g.open("test_sled")
         self._test_graph(g)
         g.close()
@@ -36,12 +36,8 @@ class StoreTestCase(unittest.TestCase):
         g.remove((EX.foo, EX.prop, EX.bar))
 
     def _test_graph(self, g: Graph):
-        g.add((EX.foo, RDF.type, EX.Entity))
-        g.add((EX.foo, EX.prop, EX.bar))
-        g.add((EX.foo, EX.prop1, Literal("foo")))
-        g.add((EX.foo, EX.prop1, Literal("foo", lang="en")))
-        g.add((EX.foo, EX.prop1, Literal("1", datatype=XSD.integer)))
-        g.remove((EX.foo, EX.prop, EX.bar))
+        self.assertIn((EX.foo, RDF.type, EX.Entity), g)
+        self.assertNotIn((EX.foo, EX.prop, EX.bar), g)
 
 
 if __name__ == "__main__":
