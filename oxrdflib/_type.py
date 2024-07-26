e1284616fbb35e5e0cd1bf83df4f4d8219d92d45
nikokaoja <nikola.vasiljevic@cognite.com>
2024-07-26 09:57:24 +0200

    remove unused enum class

--- oxrdflib/_type.py
+++ oxrdflib/_type.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from typing import Optional, Tuple
 
 from rdflib import Graph
@@ -7,14 +6,6 @@ from rdflib.term import Node
 _Triple = Tuple[Node, Node, Node]
 _Quad = Tuple[Node, Node, Node, Graph]
 _TriplePattern = Tuple[Optional[Node], Optional[Node], Optional[Node]]
-
-
-class RDFSerialization(Enum):
-    OxNTriple = "application/n-triples"
-    OxTurtle = "text/turtle"
-    OxRDFXML = "application/rdf+xml"
-    OxTriG = "application/trig"
-    OxNQuads = "application/n-quads"
 
 
 def mime_types_mapping() -> dict[str, str]:
