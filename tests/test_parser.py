4217397ee73ee4e5a51a56f18d28fd87e6f17a6c
nikokaoja <nikola.vasiljevic@cognite.com>
2024-07-26 09:49:58 +0200

    added tests

---
+++ tests/test_parser.py
@@ -0,0 +1,57 @@
+import unittest
+
+import rdflib
+
+
+class TestGraphParsing(unittest.TestCase):
+    def setUp(self):
+        self.graph = rdflib.Graph(store="Oxigraph")
+
+    def remove_triples(self):
+        self.graph.remove((None, None, None))
+
+    def test_parsing_ox_turtle_bulk_load(self):
+        self.graph.parse("./data/test.ttl", format="oxTurtle", transactional=False)
+        result = set(self.graph)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 6)
+        self.remove_triples()
+
+    def test_parsing_ox_turtle_load(self):
+        self.graph.parse("./data/test.ttl", format="oxTurtle", transactional=True)
+        result = set(self.graph)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 6)
+        self.remove_triples()
+
+    def test_parsing_ox_ntriples_bulk_load(self):
+        self.graph.parse("./data/test.n3", format="oxNTriples", transactional=False)
+        result = set(self.graph)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 6)
+        self.remove_triples()
+
+    def test_parsing_ox_ntriples_load(self):
+        self.graph.parse("./data/test.n3", format="oxNTriples", transactional=True)
+        result = set(self.graph)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 6)
+        self.remove_triples()
+
+    def test_parsing_ox_rdfxml_bulk_load(self):
+        self.graph.parse("./data/test.rdf", format="oxRdfXml", transactional=False)
+        result = set(self.graph)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 6)
+        self.remove_triples()
+
+    def test_parsing_ox_rdfxml_load(self):
+        self.graph.parse("./data/test.rdf", format="oxRdfXml", transactional=True)
+        result = set(self.graph)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 6)
+        self.remove_triples()
+
+
+if __name__ == "__main__":
+    unittest.main()
