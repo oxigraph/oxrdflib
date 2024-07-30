fcb3122d9f593a00fac2f44b4300551968f48bd5
nikokaoja <nikola.vasiljevic@cognite.com>
2024-07-30 12:05:42 +0200

    remove parsing error

--- oxrdflib/parser.py
+++ oxrdflib/parser.py
@@ -117,9 +117,6 @@ class OxigraphNQuadsParser(OxigraphParser):
         encoding: Optional[str] = None,
         **kwargs: Any,
     ) -> None:
-        if not sink.context_aware:
-            raise ParserError("OxigraphNQuadsParser must be given a context aware store.")
-
         super().parse(source, sink, format, encoding, **kwargs)
 
 
